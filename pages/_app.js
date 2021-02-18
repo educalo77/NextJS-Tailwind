71d4ecd8a96450fcfe8410b963ed6c5a549c62f6
educalo77 <educalo@outlook.com>
2021-02-18 14:03:14 -0300

    base del proyecto ok

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import "../styles/index.css";
+import Head from "next/head";
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <title key="title">Soy Desarrollador</title>
+        <meta key="description" name="description" content="Contactame!" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
-
-export default MyApp
